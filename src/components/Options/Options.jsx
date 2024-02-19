4201b3e5b5623e7c5339413e3b20b746fb44fa76
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-02-19 22:03:24 +0100

    update-options

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,13 +1,17 @@
 import css from './Options.module.css'
+import { AiOutlineLike } from "react-icons/ai";
+import { AiOutlineDislike } from "react-icons/ai";
+import { BsEmojiNeutral } from "react-icons/bs";
+import { GrPowerReset } from "react-icons/gr";
 
 const Options = ({ onUpdate, onReset, total }) => {
     return (
         <>
-            <button className={css.btn} onClick={() => onUpdate('good')}>Good</button>
-            <button className={css.btn} onClick={() => onUpdate('neutral')}>Neutral</button>
-            <button className={css.btn} onClick={() => onUpdate('bad')}>Bad</button>
+            <button className={css.btn} onClick={() => onUpdate('good')}>Good     <AiOutlineLike /> </button>
+            <button className={css.btn} onClick={() => onUpdate('neutral')}>Neutral <BsEmojiNeutral /> </button>
+            <button className={css.btn} onClick={() => onUpdate('bad')}>Bad <AiOutlineDislike /> </button>
             {total > 0 && (
-                <button className={css.btnReset} onClick={onReset}>Reset</button>
+                <button className={css.btnReset} onClick={onReset}>Reset <GrPowerReset/> </button>
             )}   
         </>
     );
