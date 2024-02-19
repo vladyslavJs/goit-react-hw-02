b94978a983137d6ee05c89fe09e3aa92ce1937f7
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-02-19 22:03:05 +0100

    update-feedback

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -4,13 +4,13 @@ const Feedback = ({ good, neutral, bad, total, positive }) => {
     return (
         <>
       {total > 0 && (
-        <>
+        <div>
           <p className={css.feedBck} >Good: {good}</p>
           <p className={css.feedBck} >Neutral: {neutral}</p>
           <p className={css.feedBck} >Bad: {bad}</p>
           <p className={css.feedBck} >Total: {total}</p>
           <p className={css.feedBck} >Positive: {positive}%</p>
-        </>
+        </div>
       )}
     </>
   );
