ba38daaf9ad28a8f8c0fdbc87a16eccb41210f3a
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-02-19 22:02:57 +0100

    update-description

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,10 +1,11 @@
 import css from './Description.module.css';
+import { SiCoffeescript } from "react-icons/si";
 
 const Description = () => {
     return (
         <>
-            <h1 className={css.title} >Sip Happens Café</h1>
-            <p className={css.description} >Please leave your feedback about our service by selecting one of the options below.
+            <h1 className={css.title} >Sip Happens Café <SiCoffeescript/></h1>
+            <p className={css.description} >«Please leave your feedback about our service by selecting one of the options below.»
             </p>
         </>
     )
